ee25582e86f1f96478289b9748a220057b34158d
fiveSecondsRealMan <fiveSecondsRealMan>
2016-08-22 18:16:29 +0800

    托盘

--- server/app.js
+++ server/app.js
@@ -12,11 +12,13 @@ const message = require('./message');
 // 控制生命周期模块 app
 // 创建浏览器窗口模块 BrowserWindow
 // 主线程向渲染线程发送和接收消息的模块 ipcMain
-const { app, BrowserWindow, ipcMain } = electron;
+const { app, BrowserWindow, ipcMain, Tray, Menu } = electron;
 
 const windowConfig = {
   width: 1365,
-  height: 980
+  height: 980,
+  icon: './icon.png',
+  title: 'my first electron application'
 };
 
 const isDevEnv = process.env.NODE_ENV === 'dev';
@@ -34,6 +36,18 @@ function getWebPageURLByEnv (isDevEnv) {
 // 创建主窗口
 function createWindow () {
   const webPageURL = getWebPageURLByEnv(isDevEnv);
+  const appIcon = new Tray('./icon.png');
+  const contextMenu = Menu.buildFromTemplate([
+    { label: 'A', type: 'radio' },
+    { label: 'B', type: 'radio' },
+    { label: 'C', type: 'radio' }
+  ]);
+
+  appIcon.on('click', (a, b, c) => {
+    mainWindow.isMinimized() && mainWindow.restore();
+  });
+  appIcon.setToolTip('this is my application');
+  appIcon.setContextMenu(contextMenu);
 
   mainWindow = new BrowserWindow(windowConfig);
 
